79f545d7fcd2562f3058987d0ffe2b9c168c5673
Aramis Razzaghipour <aramisnoah@gmail.com>
2021-11-30 19:33:39 +1100

    Add serialisation tests

--- src/proto.rs
+++ src/proto.rs
@@ -30,3 +30,202 @@ impl Serialize for Color {
             .collect_str(&format_args!("#{:02X}{:02X}{:02X}{:02X}", self.r, self.g, self.b, self.a))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use expect_test::{expect, Expect};
+    use indexmap::IndexMap;
+
+    fn check(theme: Theme, expect: Expect) {
+        expect.assert_eq(&format!("{}\n", crate::format_theme(&theme)));
+    }
+
+    #[test]
+    fn completely_empty() {
+        check(
+            Theme {
+                name: "My cool theme".to_string(),
+                textmate_rules: Vec::new(),
+                semantic_highlighting: semantic::Highlighting::Off,
+            },
+            expect![[r#"
+                {
+                    "name": "My cool theme",
+                    "tokenColors": [],
+                    "semanticHighlighting": false
+                }
+            "#]],
+        );
+    }
+
+    #[test]
+    fn empty_semantic() {
+        check(
+            Theme {
+                name: "My cool theme".to_string(),
+                textmate_rules: Vec::new(),
+                semantic_highlighting: semantic::Highlighting::On { rules: IndexMap::new() },
+            },
+            expect![[r#"
+                {
+                    "name": "My cool theme",
+                    "tokenColors": [],
+                    "semanticHighlighting": true,
+                    "semanticTokenColors": {}
+                }
+            "#]],
+        );
+    }
+
+    #[test]
+    fn basic_textmate() {
+        check(
+            Theme {
+                name: "My cool theme".to_string(),
+                textmate_rules: vec![textmate::Rule {
+                    scope: vec!["entity.function.name".to_string()],
+                    settings: textmate::RuleSettings {
+                        foreground: Color { r: 156, g: 219, b: 222, a: 255 },
+                        font_style: textmate::FontStyle::Inherit,
+                    },
+                }],
+                semantic_highlighting: semantic::Highlighting::Off,
+            },
+            expect![[r##"
+                {
+                    "name": "My cool theme",
+                    "tokenColors": [
+                        {
+                            "scope": [
+                                "entity.function.name"
+                            ],
+                            "settings": {
+                                "foreground": "#9CDBDEFF"
+                            }
+                        }
+                    ],
+                    "semanticHighlighting": false
+                }
+            "##]],
+        );
+    }
+
+    #[test]
+    fn textmate_with_font_styles() {
+        check(
+            Theme {
+                name: "My cool theme".to_string(),
+                textmate_rules: vec![textmate::Rule {
+                    scope: vec!["storage".to_string()],
+                    settings: textmate::RuleSettings {
+                        foreground: Color { r: 0, g: 0, b: 0, a: 255 },
+                        font_style: textmate::FontStyle::Set {
+                            bold: true,
+                            italic: true,
+                            underline: false,
+                        },
+                    },
+                }],
+                semantic_highlighting: semantic::Highlighting::Off,
+            },
+            expect![[r##"
+                {
+                    "name": "My cool theme",
+                    "tokenColors": [
+                        {
+                            "scope": [
+                                "storage"
+                            ],
+                            "settings": {
+                                "foreground": "#000000FF",
+                                "fontStyle": "italic bold"
+                            }
+                        }
+                    ],
+                    "semanticHighlighting": false
+                }
+            "##]],
+        );
+    }
+
+    #[test]
+    fn semantic() {
+        let mut rules = IndexMap::new();
+
+        rules.insert(
+            semantic::Selector {
+                kind: semantic::TokenKind::Wildcard,
+                modifiers: vec!["unsafe".to_string()],
+                language: None,
+            },
+            semantic::Style {
+                foreground: Color { r: 255, g: 0, b: 0, a: 255 },
+                font_styles: semantic::FontStyle {
+                    bold: semantic::FontStyleSetting::True,
+                    italic: semantic::FontStyleSetting::Inherit,
+                    underline: semantic::FontStyleSetting::Inherit,
+                },
+            },
+        );
+
+        rules.insert(
+            semantic::Selector {
+                kind: semantic::TokenKind::Specific("variable".to_string()),
+                modifiers: Vec::new(),
+                language: Some("rust".to_string()),
+            },
+            semantic::Style {
+                foreground: Color { r: 224, g: 224, b: 201, a: 255 },
+                font_styles: semantic::FontStyle {
+                    bold: semantic::FontStyleSetting::Inherit,
+                    italic: semantic::FontStyleSetting::Inherit,
+                    underline: semantic::FontStyleSetting::Inherit,
+                },
+            },
+        );
+
+        rules.insert(
+            semantic::Selector {
+                kind: semantic::TokenKind::Specific("function".to_string()),
+                modifiers: vec!["declaration".to_string(), "public".to_string()],
+                language: None,
+            },
+            semantic::Style {
+                foreground: Color { r: 156, g: 219, b: 222, a: 255 },
+                font_styles: semantic::FontStyle {
+                    bold: semantic::FontStyleSetting::Inherit,
+                    italic: semantic::FontStyleSetting::Inherit,
+                    underline: semantic::FontStyleSetting::Inherit,
+                },
+            },
+        );
+
+        check(
+            Theme {
+                name: "My cool theme".to_string(),
+                textmate_rules: Vec::new(),
+                semantic_highlighting: semantic::Highlighting::On { rules },
+            },
+            expect![[r##"
+                {
+                    "name": "My cool theme",
+                    "tokenColors": [],
+                    "semanticHighlighting": true,
+                    "semanticTokenColors": {
+                        "*.unsafe": {
+                            "foreground": "#FF0000FF",
+                            "bold": true
+                        },
+                        "variable:rust": {
+                            "foreground": "#E0E0C9FF"
+                        },
+                        "function.declaration.public": {
+                            "foreground": "#9CDBDEFF"
+                        }
+                    }
+                }
+            "##]],
+        );
+    }
+}
